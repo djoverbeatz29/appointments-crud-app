0b1eb4e34c7d767a045b6c29e79436018db118d1
jequezinho <jroverby92@gmail.com>
2022-07-20 09:09:42 -0500

    More styling, function call in HTML, more params to getApi

--- src/getApi.js
+++ src/getApi.js
@@ -1,19 +1,26 @@
-var callAPI = params =>{
+var apiPutCall = (apptType, firstName, lastName, provider, apptDate, notes) => {
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    var raw = JSON.stringify({
+        apptType,
+        firstName,
+        lastName,
+        provider,
+        apptDate,
+        notes
+    });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
-        method: 'POST',
+        method: 'PUT',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    fetch("https://26uris7n5i.execute-api.us-east-2.amazonaws.com/dev", requestOptions)
+    fetch("https://yjjszlwu3a.execute-api.us-east-2.amazonaws.com", requestOptions)
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error))
