adb2efefb2f90f224fb9b34f9184087ed4e1675d
jequezinho <jroverby92@gmail.com>
2022-07-20 13:17:19 -0500

    Updating API link

--- src/getApi.js
+++ src/getApi.js
@@ -14,13 +14,13 @@ var apiPutCall = (apptType, firstName, lastName, provider, apptDate, notes) => {
     });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
-        method: 'PUT',
+        method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    fetch("https://yjjszlwu3a.execute-api.us-east-2.amazonaws.com", requestOptions)
+    fetch("https://aoih884dsh.execute-api.us-east-2.amazonaws.com/dev", requestOptions)
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error))
